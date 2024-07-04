ded4139ebc37fb36f6db38e02c70b7dd9f64d792
Kasun_Udayanga <udayangakasun696@gmail.com>
2024-07-04 19:25:27 +0530

    aa

---
+++ Spotify/src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Sidebar() {
+  return (
+    <div className='w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex'>
+        Sidebar
+    </div>
+  )
+}
+
+export default Sidebar
