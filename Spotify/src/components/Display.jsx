f370dcabf12e2647e412f307726fe14c8b7f02f6
Rishini <nawodirishini88@gmail.com>
2024-07-05 23:14:55 +0530

    creating navigation bar

--- Spotify/src/components/Display.jsx
+++ Spotify/src/components/Display.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import DisplayHome from './DisplayHome'
 import DisplayAlbum from './DisplayAlbum'
+import { albumsData } from '../assets/assets'
 
 const Display = () => {
+
+  const displayRef = useRef();
+  const location = useLocation();
+  const isAlbum = location.pathname.includes("album");
+  
+  const albumId = isAlbum ? location.pathname.slice(-1) : "";
+  const bgColor = albumsData[Number(albumId)].bgColor;
+
+  useEffect(() => 
+  { 
+    if (isAlbum){
+      displayRef.current.style.background = `linear-gradient($ {bgColor} ,#121212)`
+    }
+    else{
+      displayRef.current.style.background = `#121212`
+    }
+  })
+
+
+
+
   return (
-    <div className='w-[100%] m2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml'> 
+    <div ref={displayRef} className='w-[100%] m2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml'> 
        <Routes>
         <Route path='/'element={<DisplayHome/>}/>
         <Route path='/album/:id'element={<DisplayAlbum/>}/>
