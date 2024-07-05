483bce4c247c116073213d9c8bbb357011772965
Kasun_Udayanga <udayangakasun696@gmail.com>
2024-07-05 13:40:33 +0530

    add home and music item

--- Spotify/src/components/Sidebar.jsx
+++ Spotify/src/components/Sidebar.jsx
@@ -4,14 +4,14 @@ import {assets} from '../assets/assets'
 function Sidebar() {
   return (
     <div className='w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex'>
-        <div className=' bg-[#121212] h[15%] rounded flex flex-col justify-around'>
+        <div className=' bg-[#121212] h-[15%] rounded flex flex-col justify-around'>
           <div className='flex items-center gap-3 pl-8 cursor-pointer'>
-            <img className=''src ={assets.home_icon} alt="" />
+            <img className='w-6'src ={assets.home_icon} alt="" />
             <p className='font-bold '>Home</p>
           </div>
 
           <div className='flex items-center p-4 gap-3 pl-8 cursor-pointer'>
-            <img className=''src ={assets.search_icon} alt="" />
+            <img className='w-6'src ={assets.search_icon} alt="" />
             <p className='font-bold '>Search</p>
           </div>
         </div>
