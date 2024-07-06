a5cb7c747859da301a7cee643eaacd3eb52a943d
Kasun_Udayanga <udayangakasun696@gmail.com>
2024-07-06 16:53:48 +0530

    change color

--- Spotify/src/components/Display.jsx
+++ Spotify/src/components/Display.jsx
@@ -24,7 +24,7 @@ const Display = () => {
   })
 
   return (
-    <div ref={displayRef} className='w-[100%] m2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml'> 
+    <div ref={displayRef} className='w-[100%] m2 px-6 pt-4 rounded bg-[#181717] text-white overflow-auto lg:w-[75%] lg:ml'> 
        <Routes>
         <Route path='/'element={<DisplayHome/>}/>
         <Route path='/album/:id'element={<DisplayAlbum/>}/>
